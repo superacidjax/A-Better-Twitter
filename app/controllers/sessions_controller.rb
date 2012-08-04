b8b26a3049df25a312b581a4cdf243ba2af2ec2d
Brian Dear <superacidjax@me.com>
2012-08-03 21:02:46 -0400

    Provides intelligent redirecting after sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ expose(:sessions)
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:error] = "I'm sorry, your email/password is incorrect!"
       render 'new'
