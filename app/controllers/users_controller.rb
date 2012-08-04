37892cf20bf86ab117e655ce9f44cf456a18acfa
Brian Dear <superacidjax@me.com>
2012-08-03 21:04:46 -0400

    Creates edit profile pages and authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if user.update_attributes(params[:user])
+      sign_in user
+      flash[:success] = "Profile updated"
+      redirect_to user
+    else
+    render 'edit'
+  end
+  end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+      redirect_to signin_path, notice: "Please sign in"
+      end
+    end
+
+    def the_right_user
+      redirect_to root_path unless current_user?(user)
+    end
 end
