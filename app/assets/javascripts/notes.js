f81ef34434e0e032ee2e0ea3753bc626ee2bf162
Brian Dear <superacidjax@me.com>
2012-09-10 22:10:28 -0400

    feed styling

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -1,5 +1,5 @@
 $('#note_content').live('keyup keydown', function(e) {
-    var maxLen = 200;
+    var maxLen = 250;
     var left = maxLen - $(this).val().length;
     $('#char-count').html(left);
   });
