d8790dcbf8fcad1b13238548e967db771dc43f5a
Brian Dear <briandear@new-host-4.home>
2012-09-12 20:33:07 -0400

    styles group creation page

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -3,3 +3,9 @@ $('#note_content').live('keyup keydown', function(e) {
     var left = maxLen - $(this).val().length;
     $('#char-count').html(left);
   });
+
+$('#group_description').live('keyup keydown', function(e) {
+    var maxLen = 250;
+    var left = maxLen - $(this).val().length;
+    $('#char-count').html(left);
+  });
--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,7 @@ class Group < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 50 }
-  validates :description, presence: true, length: { maximum: 255 }
+  validates :description, presence: true, length: { maximum: 250 }
   validates :category, presence: true, length: {maximum: 30 }
   belongs_to :user
   has_many :memberships, foreign_key: "group_membership_id", dependent: :destroy
