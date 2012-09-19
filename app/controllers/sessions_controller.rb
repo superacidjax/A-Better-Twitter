6f0640385c6a991854eb6674457331e9435420b4
Brian Dear <superacidjax@me.com>
2012-09-19 10:21:26 -0400

    adds remember me checkbox option on signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ expose(:sessions)
   def create
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
+      cookies.permanent[:remember_token] = user.remember_token
       sign_in user
       redirect_back_or root_path
     else
@@ -18,6 +19,7 @@ expose(:sessions)
   end
 
   def destroy
+    cookies.delete(:remember_token)
     sign_out
     redirect_to root_path
   end
